c2918daccf8ea968db3e9b9b2104bb49be423bd9
juego <juego@requiem4tv.com>
2011-11-28 12:33:40 +0100

    added GPL license

--- webdriver/loveMachine.py
+++ webdriver/loveMachine.py
@@ -1,6 +1,22 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+#    [loveMachine] = automatic facebook like processor
+#    Copyright (C) 2011  Julien "juego" Deswaef
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ### Config
 # Facebook credentials
 LOGIN = ""
