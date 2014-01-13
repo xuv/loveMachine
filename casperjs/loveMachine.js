b8e6b750236ec9a5a30b611a2a5d135dd5c04d8f
juego <juego@requiem4tv.com>
2014-01-13 17:04:56 +0100

    Loop over the likes seems to be stable

--- casperjs/loveMachine.js
+++ casperjs/loveMachine.js
@@ -1,6 +1,9 @@
 var numberOfLikes = 0;
-var nb = 1; 
+var nb = 1;
+var loop = 0;
+var maxLoop = 10;
 var status = ' (y)';
+var init = true;
 
 var casper = require('casper').create({
     clientScripts: [
@@ -11,7 +14,8 @@ var casper = require('casper').create({
     viewportSize: {
         width: 1024,
         height: 768
-    }
+    },
+    waitTimeout: 10000
 });
 
 
@@ -22,176 +26,22 @@ casper.start('https://facebook.com', function() {
             this.exit();
         }
     });
+    this.echo('Starting...');
 });
-
-casper.then(function(){
-    this.echo('Filling the login form');
-    this.fill('form#login_form', {
-        'email': casper.cli.get('email'),
-        'pass': casper.cli.get('password')
-    }, true);
-});
-
 
 // Catch console messages from the browser
 casper.on('remote.message', function(msg) {
     this.echo('remote message caught: ' + msg);
 });
 
-// Wait for the login page to disappear and land on the homepage
-casper.waitWhileSelector('form#login_form', function() {
-    this.echo('Logged in ;)');
-    //this.capture('fb' + (new Date()).toISOString().substr(0,19).replace(/:/g, '') + '.jpg');
-});
-
-
-casper.doSomeLove = function() {
-    if (nb > 0 ){
-        // Find all 'like' buttons, count them and mark them with a crafted id 
-        this.echo('Starting to doSomeLove');
-        this.evaluate(function(){
-            window.done = false;
-            $('a.UFILikeLink').each(function(){
-                if($(this).text() === 'Like'){
-                    $(this).attr('id', 'like' + window.nbLikes );
-                    window.nbLikes++;
-                }
-            });
-            window.done = true;
-        }); 
-
-        this.waitFor(function(){
-            return this.evaluate(function(){
-                return window.done;
-            }) === true;
-        }); 
-
-        // Get the number of Likes to process
-        nb = this.evaluate(function(){
-            return window.nbLikes;
-        }) - numberOfLikes;
-
-        this.echo('Wave of likes : ' + nb);
-        this.repeat(nb, function(){
-            this.click('#like' + numberOfLikes);
-            numberOfLikes++;
-        });
-
-        this.scrollToBottom();
-        this.wait(1000);
-
-        this.then(function(){
-            this.doSomeLove();
-        });
-
-        this.then(function(){
-            this.echo('Total likes : ' + numberOfLikes + '...');
-        });
-    }
-}
-
-casper.then(function(){
-    this.evaluate(function(){
-        window.nbLikes = 0;    
-    });
-
-    this.doSomeLove();
-});
-
-
-/*
-casper.thenEvaluate(function(){
-    window.nbLikes = 0;
-    window.done = false;
-    $('a.UFILikeLink').each(function(){
-        if($(this).text() === 'Like'){
-            $(this).attr('id', 'like' + window.nbLikes );
-            window.nbLikes++;
-        }
-    });
-    window.done = true;
-});
-
-
-
-casper.waitFor(function(){
-    return this.evaluate(function(){
-        return window.done;
-    }) === true;
-}); 
-
-casper.then(function(){
-    var nb = this.evaluate(function(){
-        return window.nbLikes;
-    });
-    this.echo('First wave of likes : ' + nb);
-    this.repeat(nb, function(){
-        this.click('#like' + numberOfLikes);
-        numberOfLikes++;
-    });
-    this.scrollToBottom();
-});
-
-casper.thenEvaluate(function(){
-   window.done = false;
-    $('a.UFILikeLink').each(function(){
-        if($(this).text() === 'Like'){
-            $(this).attr('id', 'like' + window.nbLikes );
-            window.nbLikes++;
-        }
-    });
-    window.done = true; 
-});
-
-casper.waitFor(function(){
-    return this.evaluate(function(){
-        return window.done;
-    }) === true;
-}); 
-
-casper.then(function(){
-    var nb = this.evaluate(function(){
-        return window.nbLikes;
-    }) - numberOfLikes ;
-    this.echo('Second wave of likes: ' + nb );
-    this.repeat(nb, function(){
-        this.click('#like' + numberOfLikes);
-        numberOfLikes++;
-    });
-    this.echo(numberOfLikes);
-});
-*/
-
-/*
-casper.then(function(){
-    var likes = this.getElementsInfo('a.UFILikeLink');
-    var i=0;
-    this.repeat(likes.length, function(){
-        this.echo(i + " : " + likes[i].html);
-        this.echo(likes[i].attributes.id);
-        if(likes[i].html === 'Like'){
-            this.mouse.click(likes[i].x + 5, likes[i].y + 5);
-            numberOfLikes++;
-            this.wait(500, function(){
-                this.echo(i + " : " + likes[i].html + ' (after wait)');
-            });
-        }
-
-        i++;
-    });
-});
-*/
-
-casper.then(function(){
+var postStatus = function(){
+    this.echo('Total likes : ' + numberOfLikes + '...');
     if( numberOfLikes > 0 ) {
+        this.echo('Publish status: ' + numberOfLikes + status );
+        this.click('textarea[name="xhpc_message"]');
+        this.echo('Clicked status box');
 
-        casper.then(function() {
-            this.echo('Publish status: ' + numberOfLikes + status );
-            this.click('textarea[name="xhpc_message"]');
-            this.echo('Clicked status box');
-        });
-
-        casper.waitWhileSelector('textarea[class="DOMControl_placeholder"]', function() {
+        this.waitWhileSelector('textarea[class="DOMControl_placeholder"]', function() {
             this.echo('Writting status');
             status = numberOfLikes + status;
             this.fill('form[action="/ajax/updatestatus.php"]', {
@@ -199,7 +49,7 @@ casper.then(function(){
             }, false);
         });
 
-        casper.then(function(){
+        this.then(function(){
             this.click('form[action="/ajax/updatestatus.php"] button[type="submit"]');
             this.echo('Clicked "submit"');
             // this.wait(1000);
@@ -210,14 +60,104 @@ casper.then(function(){
         // ou cliquer sur le bouton "envoyer"
 
         // Évidemment, il voit tout de suite son texte dans la page vu qu'il est encore affiché dans la zone d'écriture du texte.
-        casper.waitWhileVisible('form[action="/ajax/updatestatus.php"] button[type="submit"]', function(){
+        this.waitWhileVisible('form[action="/ajax/updatestatus.php"] button[type="submit"]', function(){
             this.echo('status written');
             var date = new Date();
             this.capture('fb' + (new Date()).toISOString().substr(0,19).replace(/:/g, '') + '.jpg');
             this.echo('Screenshot taken');
+            this.exit();
         });    
+    } else {
+        this.echo('No status posted');
+        this.exit();
     }
+} 
+
+var doSomeLove = function () {
+    if( init ){
+        this.echo('Init...');
+        this.evaluate(function(){
+            window.nbLikes = 0;    
+        });
+        init = false;
+    } 
+    if (nb > 0 && loop < maxLoop ){
+        // Find all 'like' buttons, count them and mark them with a crafted id 
+        this.then(function(){
+            this.echo('Starting to doSomeLove');
+            this.evaluate(function(){
+                window.done = false;
+                $('a.UFILikeLink').each(function(){
+                    if($(this).text() === 'Like'){
+                        $(this).attr('id', 'like' + window.nbLikes );
+                        console.log('Creating id="#like' + window.nbLikes + '"' );
+                        window.nbLikes++;
+                    }
+                });
+                window.done = true;
+            }); 
+        });
+
+        this.waitFor(function(){
+            return this.evaluate(function(){
+                return window.done;
+            }) === true;
+        }); 
+
+        this.then(function(){
+            // Get the number of Likes to process
+            nb = this.evaluate(function(){
+                return window.nbLikes;
+            }) - numberOfLikes;
+
+            this.echo('Wave of likes (nb) : ' + nb);
+        });
+
+        this.then(function(){
+            if(nb >0){
+                this.repeat(nb, function(){
+                    this.then(function(){
+                        this.click('#like' + numberOfLikes);
+                        this.echo('Clicked #like' + numberOfLikes);
+                        numberOfLikes++;
+                    });
+                });    
+            } else {
+                this.echo('Pas de loop click');
+            }    
+        });
+        
+        this.then(function(){
+            this.scrollToBottom();
+        });
+
+        this.then(function(){
+            loop++;
+            this.echo('loop: ' + loop);
+            this.run(doSomeLove);    
+        });
+        
+    } else {
+        this.then(function(){
+            this.echo('Call postStatus');
+            postStatus.call(this);
+        });
+    }
+
+}
+
+// Log into Facebook
+casper.then(function(){
+    this.echo('Filling the login form');
+    this.fill('form#login_form', {
+        'email': casper.cli.get('email'),
+        'pass': casper.cli.get('password')
+    }, true);
 });
 
+// Wait for the login page to disappear and land on the homepage
+casper.waitWhileSelector('form#login_form', doSomeLove, function(){
+    this.echo('Never reached homepage. Quitting.').exit();
+});
 
-casper.run();
+casper.run(doSomeLove);
